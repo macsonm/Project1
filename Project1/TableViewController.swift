cb0a7875447c5899864cf5061107ada47a64ec58
macsonm <75022784+macsonm@users.noreply.github.com>
2022-04-25 18:59:50 +0300

    filter by numbers in name of picture

---
+++ Project1/TableViewController.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        pictures = pictures.sorted { x1, x2 in
+            Int(x1.filter("0123456789".contains)) ?? 2 < Int(x2.filter("0123456789".contains)) ?? 3
+            //Int(x1.dropFirst(4))! > Int(x2.dropFirst(4))!
+            
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return pictures.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        //content.image = UIImage(named: pictures[indexPath.row])
+        content.text = pictures[indexPath.item]
+        cell.contentConfiguration = content
+    
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+
+    
+}
